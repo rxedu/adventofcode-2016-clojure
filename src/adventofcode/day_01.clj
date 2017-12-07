e6f07447e2819822e52f1cbeb54717959c49da51
Evan Sosenko <razorx@evansosenko.com>
2017-12-07 09:47:47 -0800

    WIP day 1

--- src/adventofcode/day_01.clj
+++ src/adventofcode/day_01.clj
@@ -1,5 +1,58 @@
 (ns adventofcode.day-01)
 
+(require 'clojure.core.matrix)
+
+(clojure.core.matrix/set-current-implementation :vectorz)
+
+(def rotations
+  {:left [[0 -1] [1 0]]
+   :right [[0 1] [-1 0]]})
+
+(def start {:dir [0, 1], :pos [0, 0]})
+
+(defn parse-move
+  [moves]
+  (let [parse-move]))
+
+(defn parse-input
+  [input]
+  (let [moves (->
+               input
+               clojure.string/trim-newline
+               (partial clojure.string/split ", ")]
+    (map parse-move moves))
+
+(defn rotate
+  "Rotates a unit vector left or right 90 degrees."
+  [direction vect]
+  (let [rotation (direction rotations)]
+    (map int (clojure.core.matrix/mmul rotation vect))))
+
+(defn move
+  "Move n steps in a direction"
+  [direction steps position]
+  (let [movement (map * (vector steps steps) direction)]
+    (map + position movement)))
+
+(defn distance
+  "Compute taxicab distance from start."
+  [position]
+  (let [offset (map - position (:pos start))]
+    (reduce + (map #(max % (- %)) offset))))
+
+(defn rotate-and-move
+  "Rotate and move."
+  ([{:keys [dir pos]} {:keys [rot steps]}]
+   (let [new-dir (rotate rot dir)
+         new-pos (move new-dir steps pos)]
+     (assoc {} :dir new-dir :pos new-pos))))
+
+; (
+;  parse-input
+;  (partial (reduce rotate-and-move start))
+;  distance
+;  )
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
--- test/adventofcode/day_01_test.clj
+++ test/adventofcode/day_01_test.clj
@@ -1,3 +1,26 @@
 (ns adventofcode.day-01-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-01 :refer :all]))
+
+(deftest parse-input-test
+  (is (= {:rot :left :steps 4} (parse-input "L4")))
+  (is (= {:rot :right :steps 3} (parse-input "R3"))))
+
+(deftest rotate-test
+  (is (= [-1 0] (rotate :left [0 1])))
+  (is (= [1 0] (rotate :right [0 1])))
+  (is (= [0 -1] (rotate :right [1 0])))
+  (is (= [0 -1] (rotate :left [-1 0]))))
+
+(deftest move-test
+  (is (= [3 0] (move [1 0] 3 [0 0])))
+  (is (= [4 -3] (move [0 -1] 2 [4 -1])))
+  (is (= [10 4] (move [-1 0] 5 [15 4]))))
+
+(deftest distance-test
+  (is (= 5 (distance [2 3])))
+  (is (= 5 (distance [-2 3])))
+  (is (= 7 (distance [0 -7]))))
+
+(deftest rotate-and-move-test
+  (is (= {:dir [-1 0] :pos [-5 0]} (rotate-and-move {:dir [0 1] :pos [0 0]} {:rot :left :steps 5}))))

6dcae5b50f5bfcc12edfbb88e4223463927331b2
Evan Sosenko <razorx@evansosenko.com>
2017-12-25 10:12:19 -0800

    Solve day 1
    Squashed commit of the following:
    
    commit 1d5294fd0bf169d3e2f660e4cf8fd013a4b199f5
    Author: Evan Sosenko <razorx@evansosenko.com>
    Date:   Mon Dec 25 09:55:05 2017 -0800
    
        Fix part 2 solution
    
    commit c7425d44895aa64e0a6b836b1cad7a493d0e597f
    Author: Evan Sosenko <razorx@evansosenko.com>
    Date:   Wed Dec 13 23:00:06 2017 -0800
    
        Attempt part 2
    
    commit 48064c783666b6f99d0c2807645e772fe6ee3535
    Author: Evan Sosenko <razorx@evansosenko.com>
    Date:   Wed Dec 13 22:32:25 2017 -0800
    
        Add trace-path
    
    commit a8be60eadcc87a2e34112ea0e21720e2e88b8b2c
    Author: Evan Sosenko <razorx@evansosenko.com>
    Date:   Tue Dec 12 23:56:51 2017 -0800
    
        WIP first attempt
    
    commit a34f623457561792f09b5727aa8907599c12f0b2
    Author: Evan Sosenko <razorx@evansosenko.com>
    Date:   Thu Dec 7 09:49:58 2017 -0800
    
        WIP
    
    commit e6f07447e2819822e52f1cbeb54717959c49da51
    Author: Evan Sosenko <razorx@evansosenko.com>
    Date:   Thu Dec 7 09:47:47 2017 -0800
    
        WIP day 1
    
    commit a464d14acd1a138a6e443c2814fab7ea53e3d8f1
    Author: Evan Sosenko <razorx@evansosenko.com>
    Date:   Wed Dec 6 23:52:33 2017 -0500
    
        Add day 01 input

--- src/adventofcode/day_01.clj
+++ src/adventofcode/day_01.clj
@@ -1,6 +1,88 @@
 (ns adventofcode.day-01)
 
+(require 'clojure.core.matrix)
+
+(clojure.core.matrix/set-current-implementation :vectorz)
+
+(def rotations
+  {:left [[0 -1] [1 0]]
+   :right [[0 1] [-1 0]]})
+
+(def start {:dir [0, 1], :pos [0, 0] :pts []})
+
+(defn parse-move
+  "Parse move into a map."
+  [move]
+  (let [dirs {"L" :left "R" :right}
+        dir (first move)
+        n (clojure.string/join (rest move))]
+    (assoc {} :rot (dirs (str dir)) :steps (Integer/parseInt n))))
+
+(defn parse-input
+  "Parse list of moves into a list of maps."
+  [input]
+  (let [moves (clojure.string/split (clojure.string/trim-newline input) #", ")]
+    (map parse-move moves)))
+
+(defn rotate
+  "Rotates a unit vector left or right 90 degrees."
+  [direction vect]
+  (let [rotation (direction rotations)]
+    (map int (clojure.core.matrix/mmul rotation vect))))
+
+(defn move
+  "Move n steps in a direction"
+  [direction steps position]
+  (let [movement (map * (vector steps steps) direction)]
+    (map + position movement)))
+
+(defn distance
+  "Compute taxicab distance from start."
+  [position]
+  (let [offset (map - position (:pos start))]
+    (reduce + (map #(max % (- %)) offset))))
+
+(defn rotate-and-move
+  "Rotate and move."
+  [{:keys [dir pos]} {:keys [rot steps]}]
+  (let [new-dir (rotate rot dir)
+        new-pts (map #(move new-dir % pos) (range 1 (inc steps)))
+        new-pos (last new-pts)]
+    (assoc {} :dir new-dir :pos new-pos :pts new-pts)))
+
+(def trace-path
+  (partial reduce #(conj %1 (rotate-and-move (last %1) %2)) [start]))
+
+(defn stop-at-first-revisit
+  [col]
+  (reduce
+   (fn [visited current]
+     (let [next (conj visited current)]
+       (if (some #{current} visited)
+         (reduced next)
+         next)))
+   col))
+
+(def parse-move-and-get-distance
+  (comp
+   distance
+   (partial :pos)
+   last
+   trace-path
+   parse-input))
+
+(def parse-move-and-get-distance-to-first-revisit
+  (comp
+   distance
+   first
+   stop-at-first-revisit
+   (partial apply concat)
+   (partial map #(get % :pts))
+   trace-path
+   parse-input))
+
 (defn solve
   "Given the input for the day, returns the solution."
   [input]
-  ())
+  [(parse-move-and-get-distance input)
+   (parse-move-and-get-distance-to-first-revisit input)])
--- test/adventofcode/day_01_test.clj
+++ test/adventofcode/day_01_test.clj
@@ -1,3 +1,31 @@
 (ns adventofcode.day-01-test
   (:require [clojure.test :refer :all]
             [adventofcode.day-01 :refer :all]))
+
+(deftest parse-move-test
+  (is (= [{:rot :left :steps 7}] (parse-input "L7")))
+  (is (= [{:rot :right :steps 2}] (parse-input "R2"))))
+
+(deftest parse-input-test
+  (is (= [{:rot :left :steps 4} {:rot :right :steps 365}]
+         (parse-input "L4, R365"))))
+
+(deftest rotate-test
+  (is (= [-1 0] (rotate :left [0 1])))
+  (is (= [1 0] (rotate :right [0 1])))
+  (is (= [0 -1] (rotate :right [1 0])))
+  (is (= [0 -1] (rotate :left [-1 0]))))
+
+(deftest move-test
+  (is (= [3 0] (move [1 0] 3 [0 0])))
+  (is (= [4 -3] (move [0 -1] 2 [4 -1])))
+  (is (= [10 4] (move [-1 0] 5 [15 4]))))
+
+(deftest distance-test
+  (is (= 5 (distance [2 3])))
+  (is (= 5 (distance [-2 3])))
+  (is (= 7 (distance [0 -7]))))
+
+(deftest rotate-and-move-test
+  (is (= {:dir [-1 0] :pos [-5 0] :pts [[-1 0] [-2 0] [-3 0] [-4 0] [-5 0]]}
+         (rotate-and-move {:dir [0 1] :pos [0 0]} {:rot :left :steps 5}))))

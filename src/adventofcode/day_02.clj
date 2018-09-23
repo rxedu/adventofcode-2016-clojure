dfd27f6b5af179ca5d3273a54dd1c560e0aad442
Evan Sosenko <razorx@evansosenko.com>
2018-09-23 10:50:31 -0700

    Solve day 2

---
+++ src/adventofcode/day_02.clj
@@ -0,0 +1,85 @@
+(ns adventofcode.day-02)
+
+(def starting-button 5)
+
+(def square-button-coordinates
+  {1 [-1, 1]
+   2 [0 1]
+   3 [1 1]
+   4 [-1 0]
+   5 [0 0]
+   6 [1 0]
+   7 [-1 -1]
+   8 [0 -1]
+   9 [1 -1]})
+
+(def star-button-coordinates
+  {1 [0, 2]
+   2 [-1 1]
+   3 [0 1]
+   4 [1 1]
+   5 [-2 0]
+   6 [-1 0]
+   7 [0 0]
+   8 [1 0]
+   9 [2 0]
+   "A" [-1 -1]
+   "B" [0 -1]
+   "C" [1 -1]
+   "D" [0 -2]})
+
+(def direction-movements
+  {"U" [0 1]
+   "D" [0 -1]
+   "L" [-1 0]
+   "R" [1 0]})
+
+(def button-coordinates square-button-coordinates)
+
+(def starting-coordinate (button-coordinates starting-button))
+(def coordinate-buttons (clojure.set/map-invert button-coordinates))
+
+(defn is-on-allowed-grid [point] (contains? coordinate-buttons point))
+
+(defn move-on-grid
+  [point movement]
+  (map + point movement))
+
+(defn move-on-allowed-grid
+  [point movement]
+  (let [new-point (move-on-grid point movement)]
+    (if (is-on-allowed-grid new-point) new-point point)))
+
+(defn move-to-button
+  [button direction]
+  (let [point (button-coordinates button)
+        movement (direction-movements direction)
+        new-point (move-on-allowed-grid point movement)]
+    (coordinate-buttons new-point)))
+
+(def move-for-sequence-to-button (partial reduce move-to-button))
+
+(defn code-reducer
+  [acc directions]
+  (let [{button :button code :code} acc
+        next-button (move-for-sequence-to-button button directions)]
+    (hash-map :button next-button :code (conj code next-button))))
+
+(defn find-code
+  [button directions]
+  (:code (reduce code-reducer {:button button :code []} directions)))
+
+(defn parse-input
+  "Parse list of directions into a list of lists"
+  [input]
+  (let [lines (clojure.string/split-lines (clojure.string/trim-newline input))]
+    (map (partial map str) lines)))
+
+(def parse-directions-and-get-code
+  (comp (partial find-code starting-button) parse-input))
+
+(defn solve
+  "Given the input for the day, returns the solution."
+  [input]
+  [(parse-directions-and-get-code input)
+   (parse-directions-and-get-code input)])
---
+++ test/adventofcode/day_02_test.clj
@@ -0,0 +1,23 @@
+(ns adventofcode.day-02-test
+  (:require [clojure.test :refer :all]
+            [adventofcode.day-02 :refer :all]))
+
+(deftest move-on-allowed-grid-test
+  (is (= [-1 -1] (move-on-allowed-grid [0 0] [-1 -1])))
+  (is (= [1 0] (move-on-allowed-grid [1 0] [1 0]))))
+
+(deftest move-to-button-test
+  (is (= 2 (move-to-button 5 "U")))
+  (is (= 2 (move-to-button 2 "U"))))
+
+(deftest move-for-sequence-to-button-test
+  (is (= 7 (move-for-sequence-to-button 5 ["U" "U" "D" "L" "L" "D"]))))
+
+(deftest find-code-test
+  (is (= [1 7] (find-code 5 [["U" "L"] ["D" "D"]]))))
+
+(deftest parse-input-test
+  (is (= [["U" "U"] ["D"] ["L" "L"]] (parse-input "UU\nD\nLL\n"))))
+
+(deftest parse-directions-and-get-code-test
+  (is (= [1 7] (parse-directions-and-get-code "UL\nDD\n"))))

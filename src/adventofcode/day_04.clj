37763eb7bbdae7837dec604aedd8a25be3ca7c36
Evan Sosenko <razorx@evansosenko.com>
2018-09-23 19:00:35 -0700

    Solve day 4 part 1

--- src/adventofcode/day_04.clj
+++ src/adventofcode/day_04.clj
@@ -12,3 +12,43 @@
               butlast)
              (string/split room #"-"))
    :hash ((comp (partial apply str) drop-last last) (string/split room #"\["))})
+
+(defn get-top-groups
+  [{hash :hash xs :letters}]
+  (let [freq (frequencies xs)
+        top-counts (set ((comp (partial take 5) reverse sort vals) freq))
+        groups (group-by
+                (fn [l] (let [n (freq l)
+                              top (contains? top-counts n)]
+                          (if top n :drop))) (set xs))]
+    (dissoc groups :drop)))
+
+(defn get-hash
+  [room]
+  (let [groups (get-top-groups room)
+        sorted-groups (into (sorted-map-by >) groups)
+        sorted-vals (map sort (vals sorted-groups))]
+    (apply str ((comp (partial take 5) flatten) sorted-vals))))
+
+(defn room?
+  [room]
+  (let [expected (get-hash room)
+        actual (:hash room)]
+    (= actual expected)))
+
+(def sum-ids
+  (let [xform (comp (filter room?) (map :id))]
+    (partial transduce xform + 0)))
+
+(def parse-rooms
+  (partial parse/map-lines parse-room))
+
+(def parse-and-sum-ids
+  (comp sum-ids parse-rooms))
+
+(defn solve
+  "Given the input for the day, returns the solution."
+  [input]
+  ((juxt
+    parse-and-sum-ids)
+   input))
--- test/adventofcode/day_04_test.clj
+++ test/adventofcode/day_04_test.clj
@@ -1,10 +1,27 @@
 (ns adventofcode.day-04-test
   (:require [clojure.test :refer [deftest is]]
             [adventofcode.day-04 :refer
-             [parse-room]]))
+             [room?
+              get-hash
+              sum-ids
+              parse-room]]))
+
+(def good-room (parse-room "aaaaa-bbb-z-y-x-123[abxyz]"))
+(def bad-room (parse-room "qqqq-bbb-zz-t-y-y-x-123[abxyz]"))
+
+(deftest get-hash-test
+  (is (= "abxyz" (get-hash good-room)))
+  (is (= "qbyzt" (get-hash bad-room))))
+
+; (deftest room?-test
+;   (is (true? (room? good-room)))
+;   (is (false? (room? bad-room))))
+
+(deftest sum-ids-test
+  (is (= 246 (sum-ids [good-room good-room bad-room]))))
 
 (deftest parse-room-test
-  (is (= {:letters ["a" "a" "a" "a" "a" "b" "b" "b" "z" "y" "x"]
+  (is (= {:letters ["a" "a" "a" "a" "a" "b" "b" "b" "z" "y" "y" "x"]
           :hash "abxyz"
           :id 123}
-         (parse-room "aaaaa-bbb-z-y-x-123[abxyz]"))))
+         (parse-room "aaaaa-bbb-z-y-y-x-123[abxyz]"))))

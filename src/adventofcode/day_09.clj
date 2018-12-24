68096e8a9e0d9e72db02e76ca2728c6e547644ea
Evan Sosenko <razorx@evansosenko.com>
2018-12-24 13:23:41 -0600

    Change decompress fn

--- src/adventofcode/day_09.clj
+++ src/adventofcode/day_09.clj
@@ -2,9 +2,9 @@
   (:require [clojure.string :as string]
             [adventofcode.parse :as parse]))
 
-(defn decompress
+(defn decompress-substr
   [num-chars num-times s]
   (let [[substr rest-s] (split-at num-chars s)
         substrs (repeat num-times (string/join substr))
         xs (reverse (conj substrs (apply str rest-s)))]
-    (string/join xs)))
+    [(string/join (butlast xs)) (last xs)]))
--- test/adventofcode/day_09_test.clj
+++ test/adventofcode/day_09_test.clj
@@ -1,7 +1,7 @@
 (ns adventofcode.day-09-test
   (:require [clojure.test :refer [deftest is]]
             [adventofcode.day-09 :refer
-             [decompress]]))
+             [decompress-substr]]))
 
-(deftest decompress-test
-  (is (= "(3x3)ABC(3x3)ABCYT" (decompress 8 2 "(3x3)ABCYT"))))
+(deftest decompress-substr-test
+  (is (= ["(3x3)ABC(3x3)ABC" "YT"] (decompress-substr 8 2 "(3x3)ABCYT"))))
